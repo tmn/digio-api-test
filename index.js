b7da53b64db056d88bd404518627856af6db2689
Tri M. Nguyen <mail@trimn.net>
2014-08-05 17:59:52 +0200

    Add calls to domains delete functionality

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ var api = new digio_api(config.token)
 // api.domains.create('tmn2.io', '191.168.0.1', function (err, data) {
 //   console.log(data);
 // })
-// 
+//
 // api.domains.get('tmn.io', function (err, data) {
+//   console.log(data);
+// })
+// 
+// api.domains.delete('tmn2.io', function (err, data) {
 //   console.log(data);
 // })
