c4c3abb70515fa128868fa5e557392b78bd2cdaf
Tri M. Nguyen <mail@trimn.net>
2014-08-05 17:40:20 +0200

    Add calls to domains functionality

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var digio_api = require('../digio-api')
 
 var api = new digio_api(config.token)
 
-api.droplets.list_droplets(function (err, data) {
-  console.log(data);
-});
+// api.droplets.list_droplets(function (err, data) {
+//   console.log(data);
+// });
+//
+// api.domains.list_all_domains(function (err, data) {
+//   console.log(data);
+// })
+//
+// api.domains.create_domain_record('tmn2.io', '191.168.0.1', function (err, data) {
+//   console.log(data);
+// })
